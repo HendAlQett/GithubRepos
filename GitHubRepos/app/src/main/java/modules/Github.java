35890d1f21499c0c7a46842bcbd07b66480b5aa5
Hend <cathe.2007@gmail.com>
2016-05-23 21:51:24 +0200

    Github object

---
+++ GitHubRepos/app/src/main/java/modules/Github.java
@@ -0,0 +1,23 @@
+package modules;
+
+/**
+ * Created by hend on 5/23/16.
+ */
+public class Github {
+
+    private String login;
+    private String blog;
+    private int public_repos;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getBlog() {
+        return blog;
+    }
+
+    public int getPublicRepos() {
+        return public_repos;
+    }
+}
