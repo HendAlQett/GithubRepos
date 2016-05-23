12cda1ce258a2c806a5fb8e03ebf9bd9068107d3
Hend <cathe.2007@gmail.com>
2016-05-23 21:51:51 +0200

    Interface for REST API calls

---
+++ GitHubRepos/app/src/main/java/retrofit/GithubService.java
@@ -0,0 +1,17 @@
+package retrofit;
+
+
+import modules.Github;
+import retrofit.http.GET;
+import retrofit.http.Path;
+import rx.Observable;
+
+/**
+ * Created by hend on 5/23/16.
+ */
+public interface GithubService {
+    String SERVICE_ENDPOINT="https://api.github.com";
+
+    @GET("users/{login}")
+    Observable<Github> getUser(@Path("login") String login);
+}
