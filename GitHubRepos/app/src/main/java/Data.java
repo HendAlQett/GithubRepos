14d051100f8766ea4cdcaadc739cc3dee3a74b21
Hend <cathe.2007@gmail.com>
2016-05-23 22:20:33 +0200

    Login users ArrayList

---
+++ GitHubRepos/app/src/main/java/Data.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hend on 5/23/16.
+ */
+public class Data {
+    public static List<String> githubList = new ArrayList<String>() {{
+        add("linkedin");
+        add("tumblr");
+        add("square");
+        add("google");
+        add("stripe");
+        add("angular");
+        add("facebook");
+        add("rails");
+    }};
+}
